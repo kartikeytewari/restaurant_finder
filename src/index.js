ce496a8d9e2993075d01b85ca8dcc4c03993189b
Kartikey Tewari <kartikeya30@gmail.com>
2021-12-23 05:26:08 +0530

    add code to populate one restaurant

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 var request = require('request');
-request('https://random-data-api.com/api/restaurant/random_restaurant', function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(body) // Print the google web page.
-     }
+var mysql = require('mysql');
+
+var mysql_connection = mysql.createConnection
+({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "restaurant_finder"
+});
+
+mysql_connection.connect(function(err)
+{
+    if (err)
+    {
+        throw err;
+    }
+    else
+    {
+        console.log("Connected to mysql server.");
+    }
+});
+
+var count=11
+request('https://random-data-api.com/api/restaurant/random_restaurant', function (error, response, body)
+{
+    if (!error && response.statusCode === 200)
+    {
+        body=JSON.parse(body)
+        console.log(body)
+
+        var sql="insert into restaurant (restaurant_id, type, description, time_monday_open, time_monday_close, time_tuesday_open, time_tuesday_close, time_wednesday_open, time_wednesday_close, time_thursday_open, time_thursday_close, time_friday_open, time_friday_close, time_saturday_open, time_saturday_close, time_sunday_open, time_sunday_close) values (?)";
+        var values=[
+            count, body["type"], body["description"], body["hours"]["monday"]["opens_at"], body["hours"]["monday"]["closes_at"], body["hours"]["tuesday"]["opens_at"], body["hours"]["tuesday"]["closes_at"], body["hours"]["wednesday"]["opens_at"], body["hours"]["wednesday"]["closes_at"], body["hours"]["thursday"]["opens_at"], body["hours"]["thursday"]["closes_at"], body["hours"]["friday"]["opens_at"], body["hours"]["friday"]["closes_at"], body["hours"]["saturday"]["opens_at"], body["hours"]["saturday"]["closes_at"], body["hours"]["sunday"]["opens_at"], body["hours"]["sunday"]["closes_at"]
+        ]
+
+        mysql_connection.query(sql, [values], function(err, result)
+        {
+            if (err)
+            {
+                throw err;
+            }
+            else
+            {
+                console.log(result.inserxtId)
+            }
+        });
+    }
+    else
+    {
+        console.log("The API returned error.")
+        console.log(error);
+    }
 })
