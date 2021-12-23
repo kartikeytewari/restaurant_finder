de8408be0cb457b06c116524111a978b775460cd
Kartikey Tewari <kartikeya30@gmail.com>
2021-12-23 05:34:11 +0530

    Add code for populating database with 100 restaurants

--- src/index.js
+++ src/index.js
@@ -21,34 +21,36 @@ mysql_connection.connect(function(err)
     }
 });
 
-var count=11
-request('https://random-data-api.com/api/restaurant/random_restaurant', function (error, response, body)
+for (let count=1;count<=100;count++)
 {
-    if (!error && response.statusCode === 200)
+    request('https://random-data-api.com/api/restaurant/random_restaurant', function (error, response, body)
     {
-        body=JSON.parse(body)
-        console.log(body)
-
-        var sql="insert into restaurant (restaurant_id, type, description, time_monday_open, time_monday_close, time_tuesday_open, time_tuesday_close, time_wednesday_open, time_wednesday_close, time_thursday_open, time_thursday_close, time_friday_open, time_friday_close, time_saturday_open, time_saturday_close, time_sunday_open, time_sunday_close) values (?)";
-        var values=[
-            count, body["type"], body["description"], body["hours"]["monday"]["opens_at"], body["hours"]["monday"]["closes_at"], body["hours"]["tuesday"]["opens_at"], body["hours"]["tuesday"]["closes_at"], body["hours"]["wednesday"]["opens_at"], body["hours"]["wednesday"]["closes_at"], body["hours"]["thursday"]["opens_at"], body["hours"]["thursday"]["closes_at"], body["hours"]["friday"]["opens_at"], body["hours"]["friday"]["closes_at"], body["hours"]["saturday"]["opens_at"], body["hours"]["saturday"]["closes_at"], body["hours"]["sunday"]["opens_at"], body["hours"]["sunday"]["closes_at"]
-        ]
-
-        mysql_connection.query(sql, [values], function(err, result)
+        if (!error && response.statusCode === 200)
         {
-            if (err)
+            body=JSON.parse(body)
+            console.log(body)
+
+            var sql="insert into restaurant (restaurant_id, type, description, time_monday_open, time_monday_close, time_tuesday_open, time_tuesday_close, time_wednesday_open, time_wednesday_close, time_thursday_open, time_thursday_close, time_friday_open, time_friday_close, time_saturday_open, time_saturday_close, time_sunday_open, time_sunday_close) values (?)";
+            var values=[
+                count, body["type"], body["description"], body["hours"]["monday"]["opens_at"], body["hours"]["monday"]["closes_at"], body["hours"]["tuesday"]["opens_at"], body["hours"]["tuesday"]["closes_at"], body["hours"]["wednesday"]["opens_at"], body["hours"]["wednesday"]["closes_at"], body["hours"]["thursday"]["opens_at"], body["hours"]["thursday"]["closes_at"], body["hours"]["friday"]["opens_at"], body["hours"]["friday"]["closes_at"], body["hours"]["saturday"]["opens_at"], body["hours"]["saturday"]["closes_at"], body["hours"]["sunday"]["opens_at"], body["hours"]["sunday"]["closes_at"]
+            ]
+
+            mysql_connection.query(sql, [values], function(err, result)
             {
-                throw err;
-            }
-            else
-            {
-                console.log(result.inserxtId)
-            }
-        });
-    }
-    else
-    {
-        console.log("The API returned error.")
-        console.log(error);
-    }
-})
+                if (err)
+                {
+                    throw err;
+                }
+                else
+                {
+                    console.log("Insertion of restaurant data succesfull " +  count)
+                }
+            });
+        }
+        else
+        {
+            console.log("The API returned error.")
+            console.log(error);
+        }
+    })
+}
