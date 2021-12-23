94a3153836b0276fc347c48a8f7b0016faf26a82
Kartikey Tewari <kartikeya30@gmail.com>
2021-12-23 07:22:24 +0530

    add logging of desired result for one day

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-var request = require('request');
+const express=require("express");
+const app=express();
+const body_parser=require("body-parser");
+const moment=require("moment")
 var mysql = require('mysql');
+
+// configure app
+app.use(body_parser.urlencoded({ extended: true }));
+
+app.get("/",function(req,res)
+{
+    console.log("Home Page")
+});
 
 var mysql_connection = mysql.createConnection
 ({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "",
     database: "restaurant_finder"
@@ -21,36 +32,64 @@ mysql_connection.connect(function(err)
     }
 });
 
-for (let count=1;count<=100;count++)
+app.get("/type/:type_id", function(req,res)
 {
-    request('https://random-data-api.com/api/restaurant/random_restaurant', function (error, response, body)
+    var day_now = new Date().getDay();;
+    var now = new moment();
+    var present_time=now.format("HH:mm:ss");
+    if (day_now == 0)
     {
-        if (!error && response.statusCode === 200)
+        // sunday
+        console.log("Sunday")
+    }
+    else if (day_now == 1)
+    {
+        // monday
+        console.log("Monday")
+    }
+    else if (day_now == 2)
+    {
+        // tuesday
+        console.log("Tuesday")
+    }
+    else if (day_now == 3)
+    {
+        // wednesday
+        console.log("Wednesday")
+    }
+    else if (day_now == 4)
+    {
+        // thursday
+        console.log("Thursday")
+        let local=[];
+        mysql_connection.query(`select name, type from restaurant where time_thursday_open<=\"${present_time}\" and \"${present_time}\"<=time_thursday_close and type=\"${req.params.type_id}\"`, function(err, result, fields)
         {
-            body=JSON.parse(body)
-            console.log(body)
-
-            var sql="insert into restaurant (restaurant_id, type, description, time_monday_open, time_monday_close, time_tuesday_open, time_tuesday_close, time_wednesday_open, time_wednesday_close, time_thursday_open, time_thursday_close, time_friday_open, time_friday_close, time_saturday_open, time_saturday_close, time_sunday_open, time_sunday_close) values (?)";
-            var values=[
-                count, body["type"], body["description"], body["hours"]["monday"]["opens_at"], body["hours"]["monday"]["closes_at"], body["hours"]["tuesday"]["opens_at"], body["hours"]["tuesday"]["closes_at"], body["hours"]["wednesday"]["opens_at"], body["hours"]["wednesday"]["closes_at"], body["hours"]["thursday"]["opens_at"], body["hours"]["thursday"]["closes_at"], body["hours"]["friday"]["opens_at"], body["hours"]["friday"]["closes_at"], body["hours"]["saturday"]["opens_at"], body["hours"]["saturday"]["closes_at"], body["hours"]["sunday"]["opens_at"], body["hours"]["sunday"]["closes_at"]
-            ]
-
-            mysql_connection.query(sql, [values], function(err, result)
+            if (err)
             {
-                if (err)
-                {
-                    throw err;
-                }
-                else
-                {
-                    console.log("Insertion of restaurant data succesfull " +  count)
-                }
-            });
-        }
-        else
-        {
-            console.log("The API returned error.")
-            console.log(error);
-        }
-    })
-}
+                throw err;
+            }
+            for (let key in result)
+            {
+                local.push(result[key]["name"]);
+            }
+            console.log(local);
+        })
+    }
+    else if (day_now == 5)
+    {
+        // friday
+        console.log("Friday")
+    }
+    else
+    {
+        // Saturday
+        console.log("Saturday")
+    }
+})
+
+// port coniguration
+const port=process.env.PORT || 8000;
+app.listen(port, process.env.IP, function()
+{
+    console.log("The blog_restful_api server have started at port " +  port);
+})
